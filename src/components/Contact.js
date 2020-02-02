79b2b63c8a68c479ae9532d246b052744f796dc7
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-02-02 21:24:15 +0000

    Landing page done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Grid, Cell} from 'react-mdl';
+import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 
 
 class Contact extends Component {
@@ -7,8 +7,57 @@ class Contact extends Component {
         return (
             <div className='contact-body'>
                 <Grid className='contact-grid'>
-                    <Cell col={6}>half page</Cell>
-                    <Cell col={6}>half page</Cell>
+                    <Cell col={6}>
+                        <h2>Colin Toft</h2>
+                        <img 
+                        src='https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png'
+                        alt='avatar'
+                        style={{height: '250px'}}
+                        />
+
+                        <p style={{width: '75%', margin:'auto', paddingTop: '1em' }}>lorem ddhwoidhwidhwioq hwqodh wiodoqw dhoqwdhwqiodh
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact me</h2>
+                        <hr/>
+                        <div className='contact-list'>
+                        <List>
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                    <i className='fa fa-phone-square' aria-hidden />
+                                    07714839855
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                    <i className='fa fa-fax' aria-hidden />
+                                    07714839855
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                    <i className='fa fa-envelope' aria-hidden />
+                                    Test@test.com
+                                </ListItemContent>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                    <i className='fa fa-skype' aria-hidden />
+                                    TestSkypeID
+                                </ListItemContent>
+                            </ListItem>
+                                                        
+                            
+                            
+
+                            
+                        </List>
+                        </div>
+                    </Cell>
                 </Grid>
             </div>
         )
